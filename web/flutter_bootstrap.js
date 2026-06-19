56f18032b243a1c52dc0d5966123094e5c0ca032
farouq nasiru <farouqnasiru@gmail.com>
2026-06-19 17:06:53 +0100

    Update web build with latest analytics and sensor fixes

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1222948388" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2772263988" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
